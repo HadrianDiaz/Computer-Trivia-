dea6e62abd99a81956bc9a172197cfe0b63401e4
Diaz <0223838@students.dadeschools.net>
2018-04-12 14:05:50 -0400

    Started

---
+++ logic.js
@@ -0,0 +1,18 @@
+var panel = $('');
+var countStartNumber = 30
+
+// Question set
+
+var question = [{
+	question: 'What was the name of the first computer?',
+	answers: ['ENIAC', 'UNIVAC 1', 'Apple 1', 'ABC'],
+	correctAnswer: 'ENIAC',
+	image: 'assets/eniac (1).jpg'
+},
+
+{
+	question: 'What was the name of the first Personal Copmuter?',
+	answers: ['ENIAC', 'UNIVAC 1', 'Apple 1', 'MITS Altair 8800'],
+	correctAnswer: 'MITS Altair 8800',
+	image: 'assets/altair8800.jpg'
+}]
